61dc63a11f8f2e17da70f049043c8871bf50674e
daye200 <daye200@naver.com>
2025-03-12 17:25:04 +0900

    build(settings): 기본세팅

---
+++ src/main/java/jombi/freemates/FreeMatesApplication.java
@@ -0,0 +1,13 @@
+package jombi.freemates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jombi/freemates/FreeMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package jombi.freemates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeMatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
