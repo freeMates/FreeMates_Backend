e9d6ff6fb371f054ba87c724d3cda9729c39891e
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-04-04 18:42:09 +0900

    프로젝트 전체 구조 개선 필요 : fix : Author 이다예 추가 https://github.com/freeMates/FreeMates_Backend/issues/12

--- src/main/java/jombi/freemates/model/constant/Author.java
+++ src/main/java/jombi/freemates/model/constant/Author.java
@@ -3,10 +3,7 @@ package jombi.freemates.model.constant;
 // Swagger DOCS에서 @ApiChangeLog 어노테이션에 사용
 public enum Author {
     SUHSAECHAN("서새찬"),
-    BAEKJIHOON("백지훈"),
-    WISEUNGJAE("위승재"),
-    KIMNAYOUNG("김나영"),
-    KIMKYUSEOP("김규섭");
+    LEEDAYE("이다예");
 
     private final String displayName;
 
