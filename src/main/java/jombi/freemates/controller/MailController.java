ce8d18be2462175d18f542153d42d3130a1c0c25
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-04-16 00:07:20 +0900

    이메일인증 기능 추가 : feat : //TODO 추가 주석 표현방법 변경, requestMapping URL 처리 "/" 추가, Loginvocation 추가, Controller단 로직 Service단 이전 : TODO: 변수이름 보강 필요, 하드코딩된 코드 변수처리필요 https://github.com/freeMates/FreeMates_Backend/issues/47

--- src/main/java/jombi/freemates/controller/MailController.java
+++ src/main/java/jombi/freemates/controller/MailController.java
@@ -1,6 +1,8 @@
 package jombi.freemates.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jombi.freemates.service.MailService;
+import jombi.freemates.util.aspect.LogMethodInvocation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+    name = "메일 API",
+    description = "메일 관련 API 제공"
+)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/mail")
@@ -17,27 +23,22 @@ public class MailController {
   private final MailService mailService;
 
 
-  //이메일 전송
-  @GetMapping("send")
+  // 이메일 전송
+  //TODO: DOCS
+  @GetMapping("/send")
+  @LogMethodInvocation
   public ResponseEntity<String> sendEmail(@RequestParam String mail) {
     mailService.sendEmail(mail);
     return ResponseEntity.ok("메일 전송을 요청하였습니다.");
   }
 
-  //이메일 인증
-  @GetMapping("verify")
-  public ResponseEntity<String> verifyEmail(@RequestParam String mail,@RequestParam Object uuidObject) {
-
-    try{
-      mailService.validateCode(mail, uuidObject);
-      return ResponseEntity.ok("인증이 완료되었습니다. 앱화면으로 돌아가 주세요");
-
-    }catch(Exception e){
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .body("오류가 발생하였습니다: " + e.getMessage());
-
-    }
-
+  // 이메일 인증
+  //TODO: DOCS
+  @GetMapping("/verify")
+  @LogMethodInvocation
+  public ResponseEntity<String> verifyEmail(
+      @RequestParam String mail,
+      @RequestParam String uuidString) {
+    return ResponseEntity.ok(mailService.validateCode(mail, uuidString));
   }
-
 }
--- src/main/java/jombi/freemates/service/MailService.java
+++ src/main/java/jombi/freemates/service/MailService.java
@@ -8,7 +8,6 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.validation.constraints.Email;
 import java.util.concurrent.TimeUnit;
-import jombi.freemates.model.dto.RegisterResponse;
 import jombi.freemates.repository.MemberRepository;
 import jombi.freemates.util.CommonUtil;
 import jombi.freemates.util.exception.CustomException;
@@ -20,8 +19,6 @@ import org.apache.commons.validator.routines.EmailValidator;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -32,84 +29,109 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Email
 public class MailService {
+
   private final JavaMailSender mailSender;
-  private static final String FROM = "${spring.mail.username}";
-  private final RedisTemplate<String, Object> redisTemplate;
+  private final RedisTemplate<String, String> redisTemplate;
   private final AuthService authService;
   private final MemberRepository memberRepository;
 
+  // Redis 이메일 코드 유효시간 : 3분
+  private static final long EMAIL_CODE_EXPIRE_MIN = 3;
 
+  // Redis 이메일 코드 key 접두사(prefix)
+  private static final String EMAIL_CODE_PREFIX = "verify:";
+
+  // 이메일 인증 BASE URL
   @Value("${freemates.host}")
   private String baseUrl;
 
-
+  // 메일 발신자
+  @Value("${mail.username}")
+  private String from;
 
   //메세지 만들기
-  private MimeMessage createMessage(String mail,Object uuidObject)throws MessagingException {
+  private MimeMessage createMessage(String mail, Object uuidObject) throws MessagingException {
     MimeMessage message = mailSender.createMimeMessage();
-    message.setFrom(new InternetAddress(FROM));
+    message.setFrom(new InternetAddress(from));
     message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mail));
     message.setSubject("freemates 이메일 인증");
 
-    String verificationUrl= baseUrl +"/api/mail/verify?mail="+mail+"&uuidObject="+uuidObject;
+    String verificationUrl = baseUrl + "/api/mail/verify?mail=" + mail + "&uuidObject=" + uuidObject;
 
     String body = "";
     body += "<h3> 다음을 눌러 인증을 완료해 주세요</h3>";
-    body += "<button style=\"padding:10px 20px; background-color:#CCF6FF; color:#fff; border:none; border-radius:5px; cursor:pointer;\" "
-        + "<p><a href='" + verificationUrl + "' style='padding:10px 20px; background-color:#007BFF; color:#fff; text-decoration:none; border-radius:5px; display:inline-block;'>인증하기</a></p>";
-    message.setText(body,"UTF-8", "html");
+    body +=
+        "<button style=\"padding:10px 20px; background-color:#CCF6FF; color:#fff; border:none; border-radius:5px; cursor:pointer;\" "
+            + "<p><a href='" + verificationUrl
+            + "' style='padding:10px 20px; background-color:#007BFF; color:#fff; text-decoration:none; border-radius:5px; display:inline-block;'>인증하기</a></p>";
+    message.setText(body, "UTF-8", "html");
     return message;
 
   }
 
+  // 메일 발송
   @Async
-  //메일 발송
   public void sendEmail(String mail) {
 
-    //유효성 검사
+    // 이메일 형식 검증
     EmailValidator validator = EmailValidator.getInstance();
     boolean isValid = validator.isValid(mail);
-    if(!isValid) {
+    if (!isValid) {
       throw new CustomException(ErrorCode.INVALID_EMAIL);
     }
 
     boolean isExist = memberRepository.existsByEmail(mail);
-    if(isExist) {
+    if (isExist) {
       throw new CustomException(ErrorCode.DUPLICATE_EMAIL);
     }
 
-
-
-    Object uuidObject = CommonUtil.createCleanUUIDString();
+    String uuidObject = CommonUtil.createCleanUUIDString();
 
     //redis에 3분간 저장
-    redisTemplate.opsForValue().set("verify:"+mail, uuidObject,3, TimeUnit.MINUTES);
-    try{
+    redisTemplate.opsForValue().set("verify:" + mail, uuidObject, EMAIL_CODE_EXPIRE_MIN, TimeUnit.MINUTES);
+    try {
       MimeMessage message = createMessage(mail, uuidObject);
-      mailSender.send(message);}
-    catch (MessagingException e) {
+      mailSender.send(message);
+    } catch (MessagingException e) {
       log.error("메일전송이 실패하였습니다", e);
       throw new MailSendException("failed to send mail", e);
     }
 
   }
 
-  //인증되었는지 확인
-  public void validateCode(String mail, Object uuidObject) {
-    //redis에서 코드 가져오기
-    Object savedUUID = redisTemplate.opsForValue().get("verify:"+mail);
-    //멤버 상태 갱신
-    boolean isValid = savedUUID != null && savedUUID.equals(uuidObject);
-    if (isValid) {
-      // Redis에서 인증 코드 삭제
-      redisTemplate.delete("verify:" + mail);
-    } else {
-      // 인증 실패 시 예외 던지기
-      ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-          .body("인증 코드가 유효하지 않거나 만료되었습니다.");
+  // 이메일 코드 인증
+  public String validateCode(String mail, String uuidString) {
+
+    final String DEFAULT_FAIL_MESSAGE = "인증 코드가 유효하지 않거나 만료되었습니다.";
+    final String DEFAULT_SUCCESS_MESSAGE = "인증이 완료되었습니다. 앱화면으로 돌아가 주세요";
+
+    try {
+      // UUIDString이 null이 아닌지 확인
+      if (uuidString == null || uuidString.isEmpty()) {
+        throw new CustomException(ErrorCode.INVALID_REQUEST);
+      }
+
+      // Redis에서 인증 코드 가져오기
+      String savedUuidString = String.valueOf(redisTemplate.opsForValue().get(EMAIL_CODE_PREFIX + mail));
+
+      // 멤버 상태 갱신
+      boolean isValidEmailCode = savedUuidString != null && savedUuidString.equals(uuidString);
+
+      if (isValidEmailCode) {
+        // Redis에서 인증 코드 삭제
+        redisTemplate.delete(EMAIL_CODE_PREFIX + mail);
+      } else {
+        // 인증 실패 시 예외 던지기
+        log.error("인증 코드가 유효하지 않거나 만료되었습니다.");
+        return DEFAULT_FAIL_MESSAGE;
+      }
+
+      // 성공 메시지 반환
+      return DEFAULT_SUCCESS_MESSAGE;
+
+    } catch (Exception e) {
+      log.error("메일 인증에 실패하였습니다", e);
+      return DEFAULT_FAIL_MESSAGE;
     }
-
   }
-
-
 }
