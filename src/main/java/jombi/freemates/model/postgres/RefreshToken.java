32a95bf3db98de8f00c144319c7c4d5fe77149f7
lulyulalla <daye200@naver.com>
2025-05-03 01:26:38 +0900

    로그인시 토큰 저장 로직 : refactor : username으로 찾던 로직 memberId으로 바꿈 https://github.com/freeMates/FreeMates_Backend/issues/53

--- src/main/java/jombi/freemates/model/postgres/RefreshToken.java
+++ src/main/java/jombi/freemates/model/postgres/RefreshToken.java
@@ -5,32 +5,42 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class RefreshToken {
   // 사용자 식별
   @Id
-  private String username;
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(updatable = false, nullable = false)
+  private UUID tokenId;
 
   // 토큰
-  @Column( nullable = false)
+  @Column(nullable = false)
   private String refreshToken;
 
-  public void update(String newToken) {
-    this.refreshToken = newToken;
+  @ManyToOne
+  private Member member;
+
+  // 생성자
+  public RefreshToken(String refreshToken, Member member) {
+    this.refreshToken = refreshToken;
+    this.member      = member;
   }
-
-
 
 
 
